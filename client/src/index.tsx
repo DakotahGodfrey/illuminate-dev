183290f8d05704bcc5551de179a706852458e2a3
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2021-08-19 13:09:06 -0400

    add proxy setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
