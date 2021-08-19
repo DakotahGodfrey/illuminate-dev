bc90cc8d27c55bc2d6722a3de0a8201c79e09f9f
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2021-08-19 13:34:32 -0400

    add heroku-prebuild script, add environement check to serve up production assets

--- server/build/keys/dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = {
-    mongoURI: 'mongodb+srv://dg_prod:mongo_prod@cluster0.gbfbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ghSecret: 'e6632735f8a9acdd1b601f6159c19c4371857821',
-    ghClient: '9f7e0645314b427a11d9',
-    ghCallback: '/auth/callback',
-};
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 const keys = require('./keys');
 const app = express();
+const path = require('path');
 
 const mongoConfig = {
   useNewUrlParser: true,
@@ -22,6 +23,15 @@ app.get('/', (req, res) => res.send({ greeting: 'hello world' }));
 require('./routes/auth')(app);
 require('./routes/api')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // allow express to serve up production assets.
+  app.use(express.static('client/build'));
+  // express can also serve up index.html if the route is unhandled
+  require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening on PORT: ${port}`);
