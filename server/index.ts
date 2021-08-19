32283313065b64b59ab906d03edab0fd571f1e48
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2021-08-19 12:06:30 -0400

    connect to mongo database

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+const keys = require('./keys');
+
+const mongoConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
 
 const app = express();
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`listening on PORT: ${port}`);
 });
